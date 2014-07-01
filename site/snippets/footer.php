5a818d387696ea82bdcd1dba0c2147df698a06f3
urimchoi <urimchoi@Urims-MacBook-Air.local>
2014-06-30 21:37:22 -0400

    Google Analytics update

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -10,6 +10,19 @@
   <?php echo js('assets/scripts/script.js') ?>
   <?php echo js('assets/scripts/animation.js') ?>
 
+
+  <!-- Google Analytics -->
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-52441712-1', 'auto');
+    ga('require', 'displayfeatures');
+    ga('send', 'pageview');
+
+  </script>
 </body>
 
 </html>
